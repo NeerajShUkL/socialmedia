6eeac0aa10bff7b45ad7661ab98a36116cc749a3
neeraj <neerajshukla8102@gmail.com>
2023-06-21 17:09:38 +0530

    posts router setup

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,7 +3,7 @@ import {
   getUser,
   getUserFriends,
   addRemoveFriends,
-} from "../contorllers/users.js";
+} from "../controllers/users.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
