858ff1c0a3b487cde7eca4d070ac568d1cd3c578
Neeraj Shukla <neerajshukla8102@gmail.com>
2023-06-23 08:54:14 +0530

    working on navbar

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,6 +1,36 @@
-import React from 'react'
+import { useState } from 'react'
+import {
+  Box,
+  IconButton,
+  InputBase,
+  Typography,
+  Select,
+  MenuItem,
+  FormControl,
+  useTheme,
+  useMediaQuery
+} from "@mui/material";
+import {
+  Search,
+  Message,
+  DarkMode,
+  LightMode,
+  Notifications,
+  Help,
+  Menu,
+  Close
+} from "@mui/icons-material";
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
 
 const Navbar = () => {
+  const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+  const dispatch = useDispatch();
+  const nevigate = useNavigate();
+  const user = useSelector(state => state.user);
+  const isNonMobileScreens = useMediaQuery("(min-width:600px)");
+
   return (
     <div>Navbar</div>
   )
