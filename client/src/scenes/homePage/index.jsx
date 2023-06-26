29baa432fd3fe136c4e2416252f48769c9cf129f
Neeraj Shukla <neerajshukla8102@gmail.com>
2023-06-26 21:56:30 +0530

    add user widget

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,13 +1,33 @@
-import { Box } from '@mui/material'
-import React from 'react'
-import Navbar from 'scenes/navbar'
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
+import Navbar from "scenes/navbar";
+import UserWidget from "scenes/widgets/UserWidget";
 
 const HomePage = () => {
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
+
   return (
     <Box>
       <Navbar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="center"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? "flex" : "block"}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+        <Box
+          flexBasis={isNonMobileScreens ? "flex" : "block"}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        ></Box>
+        {isNonMobileScreens && <Box flexBasis="26%"> </Box>}
+      </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
