6ef1a9353818c68db13e92fae9d8aeaca4feb872
Neeraj Shukla <neerajshukla8102@gmail.com>
2023-07-05 22:08:22 +0530

    update

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -2,9 +2,13 @@ import { Box, useMediaQuery } from "@mui/material";
 import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
 import UserWidget from "scenes/widgets/UserWidget";
+// import MyPostWidget from "scenes/widgets/MyPostWidget";
+// import PostsWidget from "scenes/widgets/PostsWidget";
+// import AdvertWidget from "scenes/widgets/AdvertWidget";
+// import FriendListWidget from "scenes/widgets/FriendListWidget";
 
 const HomePage = () => {
-  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
   const { _id, picturePath } = useSelector((state) => state.user);
 
   return (
@@ -14,17 +18,26 @@ const HomePage = () => {
         width="100%"
         padding="2rem 6%"
         display={isNonMobileScreens ? "flex" : "block"}
-        gap="center"
+        gap="0.5rem"
         justifyContent="space-between"
       >
-        <Box flexBasis={isNonMobileScreens ? "flex" : "block"}>
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
           <UserWidget userId={_id} picturePath={picturePath} />
         </Box>
         <Box
-          flexBasis={isNonMobileScreens ? "flex" : "block"}
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
           mt={isNonMobileScreens ? undefined : "2rem"}
-        ></Box>
-        {isNonMobileScreens && <Box flexBasis="26%"> </Box>}
+        >
+          {/* <MyPostWidget picturePath={picturePath} /> */}
+          {/* <PostsWidget userId={_id} /> */}
+        </Box>
+        {isNonMobileScreens && (
+          <Box flexBasis="26%">
+            {/* <AdvertWidget /> */}
+            <Box m="2rem 0" />
+            {/* <FriendListWidget userId={_id} /> */}
+          </Box>
+        )}
       </Box>
     </Box>
   );
--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -57,7 +57,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Happy's world
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
