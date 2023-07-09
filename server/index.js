d2e07bd90ef45c7a980ef3df3dd72118eb85c031
Neeraj Shukla <neerajshukla8102@gmail.com>
2023-07-09 10:39:27 +0530

    update

--- server/index.js
+++ server/index.js
@@ -19,11 +19,8 @@ import { verifyToken } from "./middleware/auth.js";
 // import { users, posts } from "./data/index.js";
 
 /* CONFIGURATIONS */
-console.log(import.meta.url);
 const __filename = fileURLToPath(import.meta.url);
-console.log(__filename);
 const __dirname = path.dirname(__filename);
-console.log(__dirname);
 dotenv.config();
 const app = express();
 app.use(express.json());
